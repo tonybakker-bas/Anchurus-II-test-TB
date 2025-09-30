fcdc3b5fd8f2147013189edbe799fe2927000dee
awpmbakker@gmail.com <awpmbakker@gmail.com>
2025-09-30 14:56:41 +0000

    Edited ListContexts

--- client_code/ListContexts/__init__.py
+++ client_code/ListContexts/__init__.py
@@ -13,11 +13,23 @@ import anvil.server
 
 class ListContexts(ListContextsTemplate):
   def list_contexts_refresh(self, **event_args):
-    # this function does the filling of the table contents
+    # This function does the filling of the table contents
+    # 1. call server function 'context_get', which retrieves all conrtext for the given site
     self.ContextList.items = anvil.server.call("contexts_get", Global.site_id)
+    # 2. set nr of rows per page from Global variable (which is defined by a parameter in the server-side config file)
     self.Context_list_1.rows_per_page = Global.nr_of_rows
+    # 3.save the lilst of items in the Global 'work-area' dictionary
     Global.work_area[Global.current_work_area_name]["data_list"] = self.ContextList.items
+    # Display the total number of rows 
     self.total_context_number.text = "Total number of Contexts: " + str(len(self.ContextList.items))
+  pass
+
+  def filter_columns(self, **event_args):
+    # Extract columns headers from data_list
+    if Global.work_area[Global.current_work_area_name]["data_list"]:
+      # Get the keys (which are the column headings) from the first item
+      column_headings = list(Global.work_area[Global.current_work_area_name]["data_list"][0].keys())
+
   pass
   
   def __init__(self, site_id, **properties):
