38f2f056467af55d95b036b90d93f44ee7c18441
awpmbakker@gmail.com <awpmbakker@gmail.com>
2025-10-03 17:58:24 +0000

    Created TableList

---
+++ client_code/TableList/__init__.py
@@ -0,0 +1,86 @@
+from ._anvil_designer import TableListTemplate
+from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+# import anvil.google.auth, anvil.google.drive
+# from anvil.google.drive import app_files
+from .. import Global
+import anvil.server
+
+class TableList(TableListTemplate):
+  def tablelist_refresh(self, **event_args):
+    # This function does the filling of the table contents
+    # 1. call server function '"table_type"s_get', which retrieves all rows of the table_type for the given site
+    self.rp.items = anvil.server.call("table_get",Global.site_id,Global.table_type)
+
+    # 2. set nr of rows per page from Global variable (which is defined by a parameter in the server-side config file)
+    if Global.nr_of_rows is not None:
+      self.grid.rows_per_page = Global.nr_of_rows
+
+    # 3.save the list of items in the Global 'work-area' dictionary
+    if Global.current_work_area_name is not None:
+      Global.work_area[Global.current_work_area_name]["data_list"] = (
+        self.rp.items
+      )
+    # Display the total number of rows
+    self.total_number.text = "Total number of rows: " + str(len(self.rp.items))
+
+  pass
+
+  def __init__(self, site_id, table_type, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    #
+    self.site_id = site_id
+    # Any code you write here will run before the form open
+    # Global.site_id is only None when form called from server side (e.g. printing form)
+    if Global.site_id is None:
+      # initialise some Globals variables for when the function is called from the server side
+      Global.site_id = site_id
+      Global.current_work_area_name = "TableList"
+      Global.table_type = table_type
+      Global.work_area[Global.current_work_area_name] = {}
+
+    # Create your Data Grid
+    self.grid = DataGrid()
+    # Add the Data Grid to your Form
+    self.add_component(self.grid, full_width_row=True)
+
+    # set table_type to one of "context", "find", etc
+    Global.table_type = Global.action.split(" ")[1]
+    print(Global.table_type)
+    # get the Table information form the Database
+    table_info = anvil.server.call("describe_table", Global.table_type)
+
+    # Extract the columns names from the table_info
+    # The DESCRIBE result structure is:
+    # (Field:, Type:, Null:, Key:, Default:, Extra:)
+    id = 0
+    columns_titles = []
+    for column_data in table_info:
+      # Select Column Field
+      field_name = column_data["Field"]
+      id = id + 1
+      columns_titles.append({"id": id, "title": field_name, "data_key": field_name})
+      #'text': column_name, 'id': option_id})
+
+    # assign the columns titles to the grid columns
+    self.grid.columns = columns_titles
+
+    # create the row structure of the datagrid
+    self.rp = RepeatingPanel(item_template=DataRowPanel)
+    # Set its items property
+
+    # Add the repeating panel to your data grid and set rows_per_page
+    self.grid.add_component(self.rp, full_width_row=True)
+    self.grid.rows_per_page = Global.nr_of_rows
+    
+    #???
+    Global.context_id = ""
+    # refresh the table content
+    self.tablelist_refresh()
