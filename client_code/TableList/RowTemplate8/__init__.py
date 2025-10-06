c35f736198363b2dc485c947fe23158f946a5bb8
awpmbakker@gmail.com <awpmbakker@gmail.com>
2025-10-06 20:42:15 +0000

    Edited GlobalHeader, Header, Main, TableList and 1 more

--- client_code/TableList/RowTemplate8/__init__.py
+++ client_code/TableList/RowTemplate8/__init__.py
@@ -36,6 +36,16 @@ class RowTemplate8(RowTemplate8Template):
     else:
       print("Main form not found!")
   pass
+
+  def btn_delete_click(self, **event_args):
+    """This handler is called by the dynamically created button."""
+    #print(self.item)
+    #print(Global.table_name)
+    Global.table_items = self.item
+    Global.action = "Delete " + Global.table_name.capitalize()
+    message = "You have seleted to delete " + Global.table_name.capitalize() + "\n\n" + str(self.item) + "\n\nDo you wish to continue?"
+    confirm(message)
+  pass
   
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -43,16 +53,18 @@ class RowTemplate8(RowTemplate8Template):
 
     # Any code you write here will run before the form opens.
     # create the view and edit button for the row and set button click event handlers
-    btn_view = Button(text='',align='left',icon='fa:eye',icon_align='left_edge')
+    btn_view = Button(text='',align='left',icon='fa:eye',icon_align='left_edge',tooltip="view row")
     btn_view.set_event_handler('click', self.btn_view_click)
-    btn_edit = Button(text='', align='left',icon='fa:edit',icon_align='left_edge')
-    btn_edit.set_event_handler('click', self.btn_edit_click)
     self.item['view'] = btn_view
+    btn_edit = Button(text='', align='left',icon='fa:edit',icon_align='left_edge',tooltip="edit row")
+    btn_edit.set_event_handler('click', self.btn_edit_click)
     self.item['edit'] = btn_edit
-    #self.item['view'].remove_from_parent()
-    #self.item['edit'].remove_from_parent()
+    btn_delete = Button(text='', align='left',icon='fa:remove',icon_align='left_edge',tooltip="delete row")
+    btn_delete.set_event_handler('click', self.btn_delete_click)
+    self.item['delete'] = btn_delete
 
     self.add_component(self.item['view'], column='1')
     self.add_component(self.item['edit'], column='2')
+    self.add_component(self.item['delete'], column='3')
 
 
--- client_code/TableList/__init__.py
+++ client_code/TableList/__init__.py
@@ -60,7 +60,8 @@ class TableList(TableListTemplate):
     columns_titles = []
     columns_titles.append({"id": 1, "title": "", "data_key": "view", "width": 30, "expand": True })
     columns_titles.append({"id": 2, "title": "", "data_key": "edit", "width": 30, "expand": True })
-    id = 2
+    columns_titles.append({"id": 3, "title": "", "data_key": "delete", "width": 30, "expand": True })
+    id = 3
     for column_data in table_info:
       # Select Column "Field"
       field_name = column_data["Field"]
