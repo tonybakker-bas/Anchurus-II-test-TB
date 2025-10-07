ab9b0f9693a70cf4f9b4ee8e3e81a678b9f55533
awpmbakker@gmail.com <awpmbakker@gmail.com>
2025-10-07 19:03:19 +0000

    Edited Main

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -256,12 +256,15 @@ class Main(MainTemplate):
       # when user is logged in, enable Action menu, username field and logout button, and disable content panel (welcome message)
       # also set username  to user email address
       Global.username = user["email"]
-      Global.user_role = user["role"]
+      
       #Global.DBAcontrol = anvil.server.call("check_DBAcontrol",Global.username,"i")
       #self.username.text = Global.username + "\n (" + Global.user_role + ")"
       self.username.text = Global.username
-      Global.ip_address = anvil.server.call("user_login_notification")
+      # check user authorisation - role columns will be updated in anvil user table
+      Global.ip_address = anvil.server.call("user_authorisation")
       # if users has admin role, add admin actions list and set it visible
+      user = anvil.users.get_user()
+      Global.user_role = user["role"]
       self.admin_action_list.visible = False
       if Global.user_role == "admin":
         print(Global.username, Global.user_role)
