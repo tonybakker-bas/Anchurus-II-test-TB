762bce46f5cb9ed8dfd0ef3cb6d91e67fd95d984
awpmbakker@gmail.com <awpmbakker@gmail.com>
2025-10-07 09:54:14 +0000

    Edited settings and GlobalHeader

--- client_code/GlobalHeader/__init__.py
+++ client_code/GlobalHeader/__init__.py
@@ -32,6 +32,17 @@ class GlobalHeader(GlobalHeaderTemplate):
     Global.gh_admin_list = self.Admin
     Global.gh_admin_list.visible = False
 
+    if anvil.users.get_user() is not None:
+      sites_list = anvil.server.call('sites_get_summary')
+      Global.site_options = {}
+      for x in sites_list:
+        val_list = list(x.values())
+        option = val_list[0] + " - " + val_list[1]
+        Global.site_options[option] = val_list[0]
+    else:
+      print("in SelectSite form without having logged in")
+      
+    self.select_site.items = Global.site_options.keys()
 
 
 
