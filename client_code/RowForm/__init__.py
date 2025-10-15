c353c326b25c0ce30c3dd767192918940e7db026
awpmbakker@gmail.com <awpmbakker@gmail.com>
2025-10-15 17:59:04 +0000

    Edited RowForm

--- client_code/RowForm/__init__.py
+++ client_code/RowForm/__init__.py
@@ -10,16 +10,16 @@ from anvil.tables import app_tables
 
 from .. import Global
 
-
 class RowForm(RowFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    
     # we need to find out which table we are dealing with
     print(Global.action)
     print(Global.table_name)
-
+    # get table information
+    table_fields = anvil.server.call("describe_table",Global.table_name)
     # And then we need to create all the fields based on table information 
+    # loop over table columns
