bc8885749a8af3421d7fa97e2c7782762170aa86
awpmbakker@gmail.com <awpmbakker@gmail.com>
2025-10-16 19:14:12 +0000

    Edited RowForm

--- client_code/RowForm/__init__.py
+++ client_code/RowForm/__init__.py
@@ -1,12 +1,14 @@
 from ._anvil_designer import RowFormTemplate
 from anvil import *
 import anvil.server
+import re
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+
 
 from .. import Global
 
@@ -22,8 +24,6 @@ class RowForm(RowFormTemplate):
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
     # we need to find out which table we are dealing with
-    print("Global.action = ",Global.action)
-    print("Global.table_name = ",Global.table_name)
     self.title.text = "This form is to " + Global.action
     # get table information
     table_info = anvil.server.call("describe_table",Global.table_name)
@@ -40,14 +40,18 @@ class RowForm(RowFormTemplate):
       lab = Label(text=column_name, font_size=14,tag=column_name)
       if column_type == "text":
         input = TextArea(tag=column_name)
+        len = 65535
       else:
         input = TextBox(tag=column_name)
+        match = re.search(r'.*\(+d\).*',column_type)
+        print(match)
+        len = match.group()
       input.add_event_handler('change',self.input_change)
-      row = {"header": lab, "field": input}
+      col = column_name + "(0/" + len + ")"
+      lab = Label(text=col, font_size=14,tag=column_name)
+      row = {"header": lab, "field": input, "length": len}
       self.form_fields[column_name] = row
       self.column_panel_1.add_component(lab)
       self.column_panel_1.add_component(input)
-
-    print(row)
       
 
