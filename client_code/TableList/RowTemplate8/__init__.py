c16d761526e01a984a3c386fd7b2dfae269ec43c
awpmbakker@gmail.com <awpmbakker@gmail.com>
2025-10-20 21:49:19 +0000

    Edited TableList.RowTemplate8

--- client_code/TableList/RowTemplate8/__init__.py
+++ client_code/TableList/RowTemplate8/__init__.py
@@ -27,6 +27,9 @@ class RowTemplate8(RowTemplate8Template):
     Global.work_area[Global.current_work_area_name]["menu_select_options"].visible = True
     self.parent.raise_event('x-selection-change')
     #
+    print(event_args)
+    print(self.background)
+    self.backgroud = "#66FFFF"
     if event_args["sender"].checked:
       # add row to selected list but first remove select column
       row = self.item
